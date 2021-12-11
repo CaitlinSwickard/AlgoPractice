f1da0ae4d0cb28fef1579b8fdd8c7ea14d6bafa0
Caitlin Swickard <caitlin6029@gmail.com>
2021-12-11 12:20:34 -0700

    added comments

--- twoNumSum.js
+++ twoNumSum.js
@@ -13,8 +13,10 @@ targetSum = 10;
 // two for loops
 // O(n^2) time | O(1) space
 function twoNumSum(array, targetSum) {
+  // first loop runs through the length of the array minus the last value
   for (let i = 0; i < array.length - 1; i++) {
     const firstNum = array[i];
+    // second loop runs through the entire loop of the array
     for (let j = i + 1; j < array.length; j++) {
       const secondNum = array[j];
       if (firstNum + secondNum === targetSum) {
@@ -32,11 +34,16 @@ function twoNumSum(array, targetSum) {
 // hash table
 // O(n) time | O(n) space
 function twoNumSum(array, targetSum) {
+  // Initialize  hash table
   const nums = {};
+  // iterate through array
   for (const num of array) {
-    const potnetialMatch = targetSum - num;
-    if (potnetialMatch in nums) {
-      return [potnetialMatch, num];
+    // Y = targetSum - num (num is the value in the array)
+    const potentialMatch = targetSum - num;
+    // if the Y value is present in the hash table, then return targetSum - num, num
+    if (potentialMatch in nums) {
+      return [potentialMatch, num];
+      // else store the number in to the hash table
     } else {
       nums[num] = true;
     }
@@ -48,17 +55,25 @@ function twoNumSum(array, targetSum) {
 
 
 // pointers
+// sorting the array from smallest to largest numbers
 // O(nlog(n)) time | O(1) space
 function twoNumSum(array, targetSum) {
+  // sorting array
   array.sort((a,b) => a - b);
+  // setting left pointer at index position 0
   let left = 0;
+  // setting right pointer at end of array
   let right = array.length -1;
+  // while left is less than right, add pointers together
   while (left < right) {
     const currentSum = array[left] + array[right];
+    // if currentSum = targetSum then return the two nums that equal targetSum
     if (currentSum === targetSum) {
       return [array[left], array[right]];
+      // else if currentSum is less than targetSum then move the left pointer up to next position
     } else if (currentSum < targetSum) {
       left++;
+      // else if curentSum is greater than targetSum then move the right pointer down to the next position
     } else if (currentSum > targetSum) {
       right--;
     }
